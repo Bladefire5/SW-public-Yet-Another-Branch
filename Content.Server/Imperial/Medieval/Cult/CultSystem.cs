014e5b3f206884d46c20846853d42a0184f7c7e8
BladeFire5 <angelmorel510@gmail.com>
2026-09-08 17:28:53 -0400

    Centralized passive mana changes.

--- Content.Server/Imperial/Medieval/Cult/CultSystem.cs
+++ Content.Server/Imperial/Medieval/Cult/CultSystem.cs
@@ -847,7 +847,7 @@ namespace Content.Server.Cult
                     {
                         if (TryComp<ManaComponent>(args.User, out var mana) && mana.MaxManaRaceModifier != 0)
                         {
-                            mana.Regen *= 1.25f;
+                            mana.Regen *= 12.5f;
                             Spawn("ShockWaveEffect", coords);
                             _audioSystem.PlayPvs(comp.SuccesSound, uid);
                             _chat.TrySendInGameICMessage(uid, "Скорость восстановления маны у проводящего ритуал повышена", InGameICChatType.Speak, false);
